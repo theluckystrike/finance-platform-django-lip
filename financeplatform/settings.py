2bc8b73e01fb35dcb58efa190a30a2e85bec6b94
Nicholas Daskalovic <55789531+ndaskalovic@users.noreply.github.com>
2023-06-19 13:36:06 +0200

    media files for production

--- financeplatform/settings.py
+++ financeplatform/settings.py
@@ -161,10 +161,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = "django.core.files.storage.FileSystemStorage"
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+
 django_heroku.settings(locals())
--- financeplatform/urls.py
+++ financeplatform/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +27,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT})
