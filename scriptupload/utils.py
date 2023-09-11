0fef9ebe7725e0b7d82aba4648f167515ed73e1c
Nicholas Daskalovic <55789531+ndaskalovic@users.noreply.github.com>
2023-09-11 20:28:01 +0200

    template for chart plotting - scripts are now expected to save their data to plot as a csv - backend find csv and plots the data with standard chart   formatting

--- scriptupload/utils.py
+++ scriptupload/utils.py
@@ -1,4 +1,6 @@
 import os
+import pandas as pd
+import matplotlib.pyplot as plt
 from django.conf import settings
 from financeplatform.storage_backends import PrivateMediaStorage
 from django.core.files import File
@@ -15,7 +17,8 @@ def handle_script_upload(file):
             destination.write(chunk)
 
 
-def run_script(file):
+# run the script assuming that it saves the chart image as output
+def run_script_no_processing(file):
     script_dir = os.path.dirname(file.file.name)
     os.makedirs(script_dir, exist_ok=True)
     storage = PrivateMediaStorage() if settings.USE_S3 else default_storage
@@ -32,3 +35,47 @@ def run_script(file):
         file.save(update_fields=["image"])
         i.close()
     os.chdir(settings.BASE_DIR)
+
+
+# run the script assuming that the data to be plotted has been saved by
+# the script as a csv with any name
+def run_script(file):
+    # get the script directory
+    script_dir = os.path.dirname(file.file.name)
+    # create it if it does not exist
+    os.makedirs(script_dir, exist_ok=True)
+    # select appropriate storage and open file
+    storage = PrivateMediaStorage() if settings.USE_S3 else default_storage
+    script = storage.open(file.file.name)
+    # move to script's directory and execute script
+    os.chdir(script_dir)
+    exec(script.read())
+    script.close()
+    # locate csv file that was saved by the script
+    data_to_plot = [f for f in os.listdir() if f.endswith('.csv')]
+    # if a csv file is found
+    if len(data_to_plot) > 0:
+        data = pd.read_csv(data_to_plot[-1])
+        data_file = open(data_to_plot[-1], 'rb')
+        # save to storage if it is not there already
+        if not storage.exists(os.path.join(script_dir, data_to_plot[-1])):
+            storage.save(os.path.join(script_dir, data_to_plot[-1]), File(data_file))
+        print(data.columns)
+        
+        plt.figure(figsize=(12,6))
+        data.columns = [c.capitalize() for c in data.columns]
+        # convert date string to datetime
+        data['Date'] = pd.to_datetime(data['Date'])
+        # plot 
+        plt.xlabel("Date")
+        plt.ylabel(data.columns[1])
+        plt.title("test")
+        plt.plot(data['Date'], data['Close'])
+        plt.savefig(data_to_plot[-1].replace('.csv', '.png'))
+        file.image = os.path.join(script_dir, data_to_plot[-1].replace('.csv', '.png'))
+        file.save(update_fields=["image"])
+        data_file.close()
+        # TODO: support for two y axes, standardise column names
+
+    os.chdir(settings.BASE_DIR)
+
