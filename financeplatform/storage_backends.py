786aa2492b30bebfa0f4655a6bc27d049fb1d682
Justin Rackley <justin.rackley@dal.ca>
2023-10-09 10:03:43 -0300

    Added documentation to financeplatform directory

--- financeplatform/storage_backends.py
+++ financeplatform/storage_backends.py
@@ -1,3 +1,14 @@
+"""
+Configuration for setting up Django's connection with AWS S3. The classes each describe a type of storage
+(eg. static, public, or private), and set rules for how they should behave when new files are uploaded.
+
+These rules are referenced in the settings.py file (see STATICFILES_STORAGE = 'financeplatform.storage_backends.StaticStorage')
+Which then allows them to be passed into the project configuration.
+
+For more information, see
+https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html
+"""
+
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
