b61e2e2388d9ac29a57a556c791a948405ae3c12
Nicholas Daskalovic <55789531+ndaskalovic@users.noreply.github.com>
2023-10-11 22:23:42 +0100

    remade ui in bootstrap

--- scriptupload/urls.py
+++ scriptupload/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.upload_script, name="uploadScript"),
     path('scripts/<str:scriptname>/', views.script_page, name="script"),
-    path('generate-report/', views.generate_report, name="generatePdf"),
+    path('generate-report/<int:categoryid>/', views.generate_report, name="generate_report"),
     path('scripts/<str:scriptname>/edit/', views.script_edit_page, name="script_edit"),
     path('categories/<str:categoryname>/', views.category_page, name="category"),
     path('create-category/', views.create_category, name="create_category"),
