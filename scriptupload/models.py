ec618ae5315ff654083e6d6d045a8367f90b55a9
Nicholas Daskalovic <55789531+ndaskalovic@users.noreply.github.com>
2023-11-02 21:58:04 +0000

    update reqs

--- scriptupload/models.py
+++ scriptupload/models.py
@@ -67,6 +67,10 @@ class Script(models.Model):
 
 
 class Report(models.Model):
+    """
+    Reports that have several script in them for generating
+    pdf reports
+    """
     name = models.CharField(max_length=100, unique=True)
     scripts = models.ManyToManyField(Script)
     created = models.DateTimeField(auto_now_add=True)
@@ -112,7 +116,9 @@ class IndexConstituent(models.Model):
     def __str__(self):
         return f"{self.ticker}-{self.index}"
 
-
+# TODO: could create many to many field of tickers
+# to store the stocks in each index
+# would mean renaming IndexConstituents to just stock
 class IndexAction(models.Model):
     index = models.CharField(max_length=5)
     ticker = models.CharField(max_length=5)
@@ -126,7 +132,32 @@ class IndexAction(models.Model):
     def __str__(self):
         return f"{self.ticker}-{self.index}"
 
-
 # set signals
 delete_script_files(Script)
 save_script(Script)
+
+# class Stock(models.Model):
+#     name = models.CharField(max_length=15)
+#     ticker = models.CharField(max_length=5)
+#     # no date_added
+
+# class Index(models.Model):
+#     name = models.CharField(max_length=15)
+#     ticker = models.CharField(max_length=5)
+#     stocks = models.ManyToManyField(Stock)
+
+
+# class IndexAction(models.Model):
+#     index = models.ForeignKey(Index)
+#     stock = models.ForeignKey(Stock)
+#     date = models.DateTimeField()
+#     name = models.CharField(max_length=7)
+
+# class OHLCData(models.Model):
+#     stock = models.ForeignKey(Stock) # CASCADE or something
+#     date = models.DateTimeField()
+#     open = models.DecimalField(max_digits=19, decimal_places=4)
+#     high = models.DecimalField(max_digits=19, decimal_places=4)
+#     low = models.DecimalField(max_digits=19, decimal_places=4)
+#     close = models.DecimalField(max_digits=19, decimal_places=4)
+#     volume = models.BigIntegerField()
