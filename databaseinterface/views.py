5c9cc6a30a844393d95ae4cd51b266d696e99753
Nicholas Daskalovic <55789531+ndaskalovic@users.noreply.github.com>
2023-11-22 17:13:28 +0000

    bulk upload to rest views

--- databaseinterface/views.py
+++ databaseinterface/views.py
@@ -4,27 +4,61 @@ from .serializers import OHLCSerializer, UserSerializer, IndexActionSerializer, 
 from .models import OHLCData, IndexConstituent, IndexAction, Rate
 from django.contrib.auth.models import User
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class OHLCViewSet(viewsets.ModelViewSet):
+
+    def create(self, request, *args, **kwargs):
+        is_many = isinstance(request.data, list)
+        serializer = self.get_serializer(data=request.data, many=is_many)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
     queryset = OHLCData.objects.all().order_by("date")
     serializer_class = OHLCSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 
 class IndexActionViewSet(viewsets.ModelViewSet):
+
+    def create(self, request, *args, **kwargs):
+        is_many = isinstance(request.data, list)
+        serializer = self.get_serializer(data=request.data, many=is_many)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
     queryset = IndexAction.objects.all().order_by("date")
     serializer_class = IndexActionSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 
 class IndexConstituentViewSet(viewsets.ModelViewSet):
+    def create(self, request, *args, **kwargs):
+        is_many = isinstance(request.data, list)
+        serializer = self.get_serializer(data=request.data, many=is_many)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
     queryset = IndexConstituent.objects.all().order_by("date_added")
     serializer_class = IndexConstituentSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 
 class RateViewSet(viewsets.ModelViewSet):
+    def create(self, request, *args, **kwargs):
+        is_many = isinstance(request.data, list)
+        serializer = self.get_serializer(data=request.data, many=is_many)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
     queryset = Rate.objects.all().order_by("date")
     serializer_class = RateSerializer
     permission_classes = [permissions.IsAuthenticated]
