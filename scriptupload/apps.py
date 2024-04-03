8c3dac9ee9165dc48ce68362a3e379db987d3c6b
Nicholas Daskalovic <55789531+ndaskalovic@users.noreply.github.com>
2024-04-03 17:21:53 +0100

    no script status reset on migrations

--- scriptupload/apps.py
+++ scriptupload/apps.py
@@ -3,6 +3,9 @@ Configuration for uploading new scripts to the project.
 """
 
 from django.apps import AppConfig
+import sys
+import logging
+logger = logging.getLogger('testlogger')
 
 
 class ScriptuploadConfig(AppConfig):
@@ -11,11 +14,15 @@ class ScriptuploadConfig(AppConfig):
 
     def ready(self):
         import scriptupload.signals
-        # from .models import Report, Script
-        # # reset runs that were interrupted by crash or other
-        # for report in Report.objects.filter(status="running"):
-        #     report.status = "success"
-        #     report.save(update_fields=["status"])
-        # for script in Script.objects.filter(status=Script.ExecutionStatus.RUNNING):
-        #     script.set_status(Script.ExecutionStatus.FAILURE,
-        #                       "Please try again")
+        # execute only if not running migrations
+        if 'migrate' not in sys.argv:
+            from .models import Report, Script
+            # reset runs that were interrupted by crash or other
+            for report in Report.objects.filter(status="running"):
+                report.status = "success"
+                report.save(update_fields=["status"])
+            for script in Script.objects.filter(status=Script.ExecutionStatus.RUNNING):
+                script.set_status(Script.ExecutionStatus.FAILURE,
+                                  "Please try again")
+            logger.info(
+                f"[ScriptuploadConfig] Resetting interrupted runs. Argv was {sys.argv}")
