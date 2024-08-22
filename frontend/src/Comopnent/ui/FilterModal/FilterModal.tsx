84e2b84aa483bc269bcee92a8620d53fa3ff62ba
Ankush <fullstack.mspl@gmail.com>
2024-08-22 15:35:31 +0530

    latest code

--- frontend/src/Comopnent/ui/FilterModal/FilterModal.tsx
+++ frontend/src/Comopnent/ui/FilterModal/FilterModal.tsx
@@ -1,29 +1,33 @@
-import { FC } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import { FC } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 interface FilterModalProps {
- 
   show: boolean;
   handleClose: () => void;
 }
 
-const FilterModal: FC<FilterModalProps> = ({show, handleClose }) => {
+const FilterModal: FC<FilterModalProps> = ({ show, handleClose }) => {
   return (
     <>
-      <Modal  size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered show={show} onHide={handleClose}>
-       
-        <Modal.Body className='bg-light-green' style={{
-          borderRadius:'25px',
-          overflow:'hidden'
-        }}> 
-
-        <form  method="post" encType="multipart/form-data">
-          <div className="mb-3">
-            <div className="row mx-0 px-5">
-              <div className="col-6 m-0 px-4">
+      <Modal
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show={show}
+        onHide={handleClose}
+      >
+        <Modal.Body
+          className="bg-light-green"
+          style={{
+            borderRadius: "25px",
+            overflow: "hidden",
+          }}
+        >
+          <form method="post" encType="multipart/form-data">
+            <div className="mb-3">
+              <div className="row mx-0 px-5">
+                {/* <div className="col-lg-6 m-0 px-4 mb-3">
             <label htmlFor="category" className="form-label">Category</label>
                 <select
                   id="category"
@@ -44,7 +48,7 @@ const FilterModal: FC<FilterModalProps> = ({show, handleClose }) => {
 
                 </select>
               </div>
-            <div className="col-6 m-0 px-4">
+            <div className="col-lg-6 m-0 px-4 mb-3">
             <label htmlFor="category" className="form-label">Add Script</label>
                 <select
                   id="category"
@@ -65,7 +69,7 @@ const FilterModal: FC<FilterModalProps> = ({show, handleClose }) => {
 
                 </select>
               </div>
-              <div className="col-6 m-0 px-4">
+              <div className="col-lg-6 m-0 px-4 mb-3">
             <label htmlFor="category" className="form-label">Sub-Category</label>
                 <select
                   id="category"
@@ -85,29 +89,88 @@ const FilterModal: FC<FilterModalProps> = ({show, handleClose }) => {
                   <option value="Breadth">Breadth</option>
 
                 </select>
-              </div>
-            <div className="col-6 m-0 px-4">
-            <label htmlFor="category" className="form-label">Sub-Category 2</label>
-                <select
-                  id="category"
-                  name="category"
-                  className="form-select m-0"
-                  
-                  required
-                >
-                  <option value="" disabled selected>All</option>
-                  <option value="Returns">Returns</option>
-                  <option value="USD">USD</option>
-
-                  <option value="Bonds">Bonds</option>
-
-                  <option value="CAD">CAD</option>
-
-                  <option value="Breadth">Breadth</option>
-
-                </select>
-              </div>
-              <div className="col-6 m-0 px-4">
+              </div> */}
+                <div className="col-lg-6 m-0 px-4 mb-3">
+                  <div className="dropdown">
+                    <label htmlFor="">Category</label>
+                    <input type="text" placeholder="All" />
+                    <div className="dropdown-content">
+                      <a>Returns</a>
+                      <a>USD</a>
+                      <a>Bonds</a> <a>CAD</a>{" "}
+                      <a>Breadth</a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6 m-0 px-4 mb-3">
+                  <div className="dropdown">
+                    <label htmlFor="">Sub-Category</label>
+                    <input type="text" placeholder="All" />
+                    <div className="dropdown-content">
+                      <a>Returns</a>
+                      <a>USD</a>
+                      <a>Bonds</a> <a>CAD</a>{" "}
+                      <a>Breadth</a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6 m-0 px-4 mb-3">
+                  <div className="dropdown">
+                    <label htmlFor="">Sub-Category 2</label>
+                    <input type="text" placeholder="All" />
+                    <div className="dropdown-content">
+                      <a>Returns</a>
+                      <a>USD</a>
+                      <a>Bonds</a> <a>CAD</a>{" "}
+                      <a>Breadth</a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6 m-0 px-4 mb-3">
+                  <div className="dropdown">
+                    <label htmlFor="">Added</label>
+                    <input type="text" placeholder="All" />
+                    <div className="dropdown-content">
+                      <a>Returns</a>
+                      <a>USD</a>
+                      <a>Bonds</a> <a>CAD</a>{" "}
+                      <a>Breadth</a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6 m-0 px-4 mb-3">
+                  <div className="dropdown">
+                    <label htmlFor="">Last Updated</label>
+                    <input type="text" placeholder="All" />
+                    <div className="dropdown-content">
+                      <a>Returns</a>
+                      <a>USD</a>
+                      <a>Bonds</a> <a>CAD</a>{" "}
+                      <a>Breadth</a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6 m-0 px-4 mb-3  dropdown">
+                  <label htmlFor="" className="invisible">bh</label> 
+                  <div className="d-flex justify-content-between">
+                  <button
+                    onClick={handleClose}
+                    className="btn btn-light  border-2px col-5 mx-auto   fw-bold  "
+                    type="button"
+                  >
+                    Reset
+                  </button>
+                  <button
+                    onClick={handleClose}
+                    className="btn btn-dark   col-5 mx-auto   fw-bold  "
+                    type="button"
+                  >
+                    Apply
+                  </button>
+                  </div>
+                  <div />
+                </div>
+                {/* <div className="col-lg-6 m-0 px-4 mb-3">
             <label htmlFor="category" className="form-label">Added</label>
                 <select
                   id="category"
@@ -128,7 +191,7 @@ const FilterModal: FC<FilterModalProps> = ({show, handleClose }) => {
 
                 </select>
               </div>
-            <div className="col-6 row m-0 px-4">
+            <div className="col-lg-6 row m-0 px-4">
             <label htmlFor="category" className="form-label invisible">Last Updated</label>
 
             <button
@@ -147,13 +210,10 @@ const FilterModal: FC<FilterModalProps> = ({show, handleClose }) => {
               >
                Apply
               </button>
-              </div>  
-         
+              </div>   */}
+              </div>
             </div>
-          </div>
- 
-        </form>
-
+          </form>
         </Modal.Body>
         {/* <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
@@ -166,6 +226,6 @@ const FilterModal: FC<FilterModalProps> = ({show, handleClose }) => {
       </Modal>
     </>
   );
-}
+};
 
 export default FilterModal;
