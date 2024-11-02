3c14e5a6d3a09e003e0c273bc3ee95695a170e02
Nicholas Daskalovic <55789531+ndaskalovic@users.noreply.github.com>
2024-11-02 21:41:23 +0100

    comments

--- backend/scriptupload/utils/summary/summary.py
+++ backend/scriptupload/utils/summary/summary.py
@@ -2,31 +2,34 @@ import pandas as pd
 from scriptupload.models import Script
 
 
-def get_date_col(script):
+def get_date_col(script) -> str | None:
+    '''Get the first column in a script's table data which is a date type'''
     for col in script.table_data.table_meta['columns']:
         if col['type'] == "date":
             return col['name']
     return None
 
 
-def make_summary_table(summary) -> pd.DataFrame:
+def make_summary_table(summary) -> tuple[pd.DataFrame, dict]:
     meta = summary.meta.get("scripts", None)
     if meta is None:
         raise Exception(f"Meta on summary {summary.id} is empty")
 
     script_ids = [int(k) for k in meta.keys()]
     first_script = Script.objects.get(id=script_ids[0])
+    # init df with first script data
     summary_df = pd.read_csv(first_script.table_data_file)
     date_col_name = get_date_col(first_script)
     if date_col_name is None:
         raise Exception(f"No date column found in script {first_script.id}")
+    # convert data column to datetime and change name
     summary_df[date_col_name] = pd.to_datetime(summary_df[date_col_name])
-
     summary_df = summary_df[[date_col_name,
                              meta[str(first_script.id)]["table_col_name"]]]
     date_index = date_col_name
     summary_df[date_col_name].name = date_index
 
+    # for each script, merge it into the df on matching dates
     for sid in script_ids[1:]:
         script = Script.objects.get(id=sid)
         date_col_name = get_date_col(script)
@@ -38,8 +41,10 @@ def make_summary_table(summary) -> pd.DataFrame:
         summary_df = pd.merge(
             summary_df, sdf, left_on=date_index, right_on=date_col_name, how="inner")
 
+    # get sum of all cols and sort by date index descending
     summary_df['total'] = summary_df[summary_df.columns[1:]].sum(axis=1)
     summary_df = summary_df.sort_values(date_index, ascending=False)
+    # update meta last value
     for sid in script_ids:
         meta[str(sid)]["table_col_last_value"] = float(summary_df[meta[str(
             sid)]["table_col_name"]].iloc[0])
