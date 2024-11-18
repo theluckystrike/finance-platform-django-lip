6b68dded2bec3d6a54aba90c27b0af67ae633597
Nicholas Daskalovic <55789531+ndaskalovic@users.noreply.github.com>
2024-11-18 15:42:00 +0000

    tf fmt

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,24 +5,24 @@ output "ecs_task_execution_role_arn" {
 
 output "subnet1" {
   description = "Public subnet 1"
-  value = aws_subnet.public_subnet_1.id
+  value       = aws_subnet.public_subnet_1.id
 }
 output "subnet2" {
   description = "Public subnet 2"
-  value = aws_subnet.public_subnet_2.id
+  value       = aws_subnet.public_subnet_2.id
 }
 
 output "security_group" {
   description = "ECS security group ID"
-  value = aws_security_group.ecs_security_group.id
+  value       = aws_security_group.ecs_security_group.id
 }
 
 output "alb_hostname" {
   description = "Load balancer URL"
-  value = aws_lb.production.dns_name
+  value       = aws_lb.production.dns_name
 }
 
 output "route_53_nameservers" {
   description = "Route 53 Zone nameservers"
-  value = aws_route53_zone.main.name_servers
+  value       = aws_route53_zone.main.name_servers
 }
