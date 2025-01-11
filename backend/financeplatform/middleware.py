2bf83332a91d320df1c5cbbbc2d4e331399639b7
Nicholas Daskalovic <55789531+ndaskalovic@users.noreply.github.com>
2025-01-11 13:05:07 +0100

    remove logging in health check

--- backend/financeplatform/middleware.py
+++ backend/financeplatform/middleware.py
@@ -8,5 +8,4 @@ logger = logging.getLogger('testlogger')
 class HealthCheckMiddleware(MiddlewareMixin):
     def process_request(self, request):
         if request.META['PATH_INFO'] == '/ping/':
-            logger.info(f"[Health Check] Received health check")
             return HttpResponse('pong!')
