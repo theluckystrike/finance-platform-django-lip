fec03a0875f6e9e2030bb2715c5a62431f32ac60
Nicholas Daskalovic <55789531+ndaskalovic@users.noreply.github.com>
2025-02-16 20:22:53 +0000

    update default ticker for model performance to be S&P 500 index

--- backend/olandinvestmentsapi/models.py
+++ backend/olandinvestmentsapi/models.py
@@ -25,7 +25,7 @@ class Summary(models.Model):
     name = models.CharField(max_length=100, unique=True)
     created = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(blank=True, null=True)
-    ticker = models.CharField(default='AAPL', null=True, blank=True, max_length=10) #TODO: temprory hard-coded!
+    ticker = models.CharField(default='^SPX', null=True, blank=True, max_length=10) #TODO: temprory hard-coded!
     # timeseries_start_date = models.DateField()
     # timeseries_end_date = models.DateField(null=True, blank=True)
     '''
