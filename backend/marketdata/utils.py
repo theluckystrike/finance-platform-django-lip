6172f29fbafea218c8206dc3ad1ff8764c2b411c
Nicholas Daskalovic <55789531+ndaskalovic@users.noreply.github.com>
2025-03-08 13:07:54 +0100

    add new ohlc data collection to daily schedule task

--- backend/marketdata/utils.py
+++ backend/marketdata/utils.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+from marketdata.models import OHLCTimeSeries
 
 
 def yf_has_symbol(asset: str) -> bool:
@@ -9,3 +10,21 @@ def yf_has_symbol(asset: str) -> bool:
         period='1mo',
         interval='1d')
     return len(info) > 0
+
+
+def yf_get_all_historic_data(ticker: str) -> List[OHLCTimeSeries]:
+    """
+    Fetches all historic data for a given ticker.
+    """
+    data = yf.Ticker(ticker).history(period='max', interval='1d')
+    ohlc_data = []
+    for index, row in data.iterrows():
+        ohlc_data.append(OHLCTimeSeries(
+            date=row['Date'].strftime('%Y-%m-%d'),
+            open=row['Open'],
+            high=row['High'],
+            low=row['Low'],
+            close=row['Close'],
+            volume=row['Volume']
+        ))
+    return ohlc_data
